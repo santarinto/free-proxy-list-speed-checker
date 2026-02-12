70e09ab4803aa87d2eef0cc4c2c09ada5aab06a7
Glazkov V Ilya aka Santarinto <santarinto@gmail.com>
2026-02-12 23:36:19 +0300

    add command-line interface with usage instructions and basic commands

--- main.go
+++ main.go
@@ -3,12 +3,58 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
 
 	"free-proxy-list-speed-checker/internal/cache"
 	"free-proxy-list-speed-checker/internal/config"
 )
 
+func printUsage() {
+	fmt.Println("Free Proxy List Speed Checker")
+	fmt.Println("\nUsage:")
+	fmt.Println("  program <command> [arguments]")
+	fmt.Println("\nCommands:")
+	fmt.Println("  list")
+	fmt.Println("      List all available proxy server collections")
+	fmt.Println()
+	fmt.Println("  scan <collection_name>")
+	fmt.Println("      Scan a proxy server collection for speed testing")
+	fmt.Println("      Arguments:")
+	fmt.Println("        collection_name - Name of the collection (default: socks5)")
+	fmt.Println()
+	fmt.Println("  stats <collection_name>")
+	fmt.Println("      Display available speed information for a collection")
+	fmt.Println("      Arguments:")
+	fmt.Println("        collection_name - Name of the collection (default: socks5)")
+	fmt.Println()
+	fmt.Println("  get-fast <collection_name> <number>")
+	fmt.Println("      Get the fastest proxy servers from a collection")
+	fmt.Println("      Arguments:")
+	fmt.Println("        collection_name - Name of the collection (default: socks5)")
+	fmt.Println("        number          - Number of proxies to retrieve (default: 1)")
+	fmt.Println()
+	fmt.Println("  clear")
+	fmt.Println("      Clear the cache")
+	fmt.Println()
+	fmt.Println("Examples:")
+	fmt.Println("  program list")
+	fmt.Println("  program scan socks5")
+	fmt.Println("  program stats")
+	fmt.Println("  program get-fast socks5 5")
+	fmt.Println("  program clear")
+}
+
 func main() {
+	if len(os.Args) < 2 {
+		printUsage()
+		os.Exit(0)
+	}
+
+	command := os.Args[1]
+
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatal(err)
@@ -19,6 +65,55 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("App name: %s\nSource repo URL: %s\n", cfg.AppName, cfg.SourceRepoUrl)
-	fmt.Printf("Cache directory: %s\n", c.Dir)
+	switch command {
+	case "list":
+		fmt.Println("Available proxy collections:")
+		v := reflect.ValueOf(cfg.ProxyCollectionList)
+		t := v.Type()
+		for i := 0; i < v.NumField(); i++ {
+			field := t.Field(i)
+			tagValue := field.Tag.Get("toml")
+			if tagValue != "" {
+				fmt.Printf("  - %s\n", tagValue)
+			} else {
+				fmt.Printf("  - %s\n", strings.ToLower(field.Name))
+			}
+		}
+
+	case "scan":
+		collection := "socks5"
+		if len(os.Args) > 2 {
+			collection = os.Args[2]
+		}
+		fmt.Printf("Scanning collection: %s\n", collection)
+
+	case "stats":
+		collection := "socks5"
+		if len(os.Args) > 2 {
+			collection = os.Args[2]
+		}
+		fmt.Printf("Displaying stats for collection: %s\n", collection)
+
+	case "get-fast":
+		collection := "socks5"
+		number := 1
+		if len(os.Args) > 2 {
+			collection = os.Args[2]
+		}
+		if len(os.Args) > 3 {
+			if n, err := strconv.Atoi(os.Args[3]); err == nil {
+				number = n
+			}
+		}
+		fmt.Printf("Getting %d fastest proxy(s) from collection: %s\n", number, collection)
+
+	case "clear":
+		fmt.Println("Clearing cache...")
+
+	default:
+		fmt.Printf("Unknown command: %s\n\n", command)
+		fmt.Printf("\nCache directory: %s\n", c.Dir)
+		printUsage()
+		os.Exit(1)
+	}
 }
