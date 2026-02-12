f0605d500c95ca4e68ce4c234956b59230fa1dda
Glazkov V Ilya aka Santarinto <santarinto@gmail.com>
2026-02-12 22:54:23 +0300

    add support for temporary cache directory creation when using system temp path

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 type Cache struct {
@@ -16,6 +17,15 @@ func New(cacheDir string) (*Cache, error) {
 	}
 
 	clean := filepath.Clean(cacheDir)
+
+	if clean == filepath.Clean(os.TempDir()) {
+		prefix := fmt.Sprintf("speed-checker-%s-", time.Now().Format("20060102T150405Z"))
+		dir, err := os.MkdirTemp(os.TempDir(), prefix)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create temporary cache directory: %w", err)
+		}
+		return &Cache{dir}, nil
+	}
 
 	abs, err := filepath.Abs(clean)
 	if err != nil {
